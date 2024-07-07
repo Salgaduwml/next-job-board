d557169ba5f841ee6bd5302c2a617e5b3a3231f6
Malinda <salgaduwml@gmail.com>
2024-07-07 17:03:04 +0530

    setup prisma and filters

---
+++ src/components/JobResults.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import JobListItem from "./JobListItem";
+import prisma from "@/lib/prisma";
+import { jobFilterValues } from "@/lib/validation";
+import { Prisma } from "@prisma/client";
+
+interface JobResultsProps {
+  filterValues: jobFilterValues;
+}
+
+const JobResults = async ({
+  filterValues: { q, type, location, remote },
+}: JobResultsProps) => {
+  const searchString = q
+    ?.split(" ")
+    .filter((word) => word.length > 1)
+    .join(" & ");
+
+  const searchFilter: Prisma.JobWhereInput = searchString
+    ? {
+        OR: [
+          { title: { search: searchString } },
+          { companyName: { search: searchString } },
+          { type: { search: searchString } },
+          { location: { search: searchString } },
+          { locationType: { search: searchString } },
+        ],
+      }
+    : {};
+
+  const where: Prisma.JobWhereInput = {
+    AND: [
+      searchFilter,
+      type ? { type } : {},
+      location ? { location } : {},
+      remote ? { locationType: "Remote" } : {},
+      { approved: true },
+    ],
+  };
+
+  const jobs = await prisma.job.findMany({
+    where: where,
+    orderBy: { createdAt: "desc" },
+  });
+  return (
+    <div className="grow space-y-4">
+      {jobs.map((job) => (
+        <JobListItem job={job} key={job.id} />
+      ))}
+      {jobs.length === 0 && (
+        <p className="m-auto text-center">
+          No jobs found. Try adjusting your filters.
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default JobResults;
